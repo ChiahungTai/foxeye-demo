ca2f87a9ac5fa42eaf29be8a268fb9805432cec1
Kaku Kuo <kakukogou@gmail.com>
2015-06-01 15:21:50 +0800

    Sample_Monitor v2

--- Sample_Monitor/control_worker.js
+++ Sample_Monitor/control_worker.js
@@ -10,11 +10,7 @@ onmessage = function(event) {
 			if (event.data.type == "display_arraybuffer") {
 				// send back to the main thread
 				postMessage({"type":event.data.type,
-										 "buffer":event.data.buffer,
-										 "bufferLength":event.data.bufferLength,
-										 "imageWidth":event.data.imageWidth,
-										 "imageHeight":event.data.imageHeight},
-										[event.data.buffer]);
+										 "bitmap":event.data.bitmap});
 				isProcessing = false;
 			}
 		};
@@ -24,42 +20,28 @@ onmessage = function(event) {
 }
 
 var frameNum = 0;
+var dropNum = 0;
+
+function showDropRate() {
+	console.log("Drop rate = " + 100 * dropNum / frameNum + "%");
+}
 
 onvideoprocess = function(event) {
+	if (frameNum % 100 == 1) {
+		showDropRate();
+	}
+
 	frameNum++;
 	if (isProcessing) {
 		// drop this frame
-		console.log("drop frame[" + frameNum + "]")
+		console.log("drop frame[" + frameNum + "]");
+		dropNum++;
 		return;
 	} else {
-		console.log("process frame[" + frameNum + "]")
+		// console.log("process frame[" + frameNum + "]")
 		isProcessing = true;
-		var bitmap = event.inputImageBitmap;
-		var bitmapFormat = bitmap.findOptimalFormat();;
-		var bitmapBufferLength = bitmap.mappedDataLength(bitmapFormat);;
-		var bitmapBuffer = new ArrayBuffer(bitmapBufferLength);
-		var bitmapPixelLayout = bitmap.mapDataInto(bitmapFormat, bitmapBuffer, 0, bitmapBufferLength);
 		// send to the process worker
 		processWorker.postMessage({"type":"convert_color",
-  														 "ywidth":bitmapPixelLayout.channels[0].width,
-															 "yheight":bitmapPixelLayout.channels[0].height,
-															 "yoffset":bitmapPixelLayout.channels[0].offset,
-															 "ystride":bitmapPixelLayout.channels[0].stride,
-															 "yskip":bitmapPixelLayout.channels[0].skip,
-															 "uwidth":bitmapPixelLayout.channels[1].width,
-															 "uheight":bitmapPixelLayout.channels[1].height,
-															 "uoffset":bitmapPixelLayout.channels[1].offset,
-															 "ustride":bitmapPixelLayout.channels[1].stride,
-															 "uskip":bitmapPixelLayout.channels[1].skip,
-															 "vwidth":bitmapPixelLayout.channels[2].width,
-															 "vheight":bitmapPixelLayout.channels[2].height,
-															 "voffset":bitmapPixelLayout.channels[2].offset,
-															 "vstride":bitmapPixelLayout.channels[2].stride,
-															 "vskip":bitmapPixelLayout.channels[2].skip,
-															 "imageWidth":bitmap.width,
-															 "imageHeight":bitmap.height,
-															 "bitmapBuffer":bitmapBuffer,
-															 "bitmapBufferLength":bitmapBufferLength},
-															[bitmapBuffer]);
+  														 "bitmap":event.inputImageBitmap});
 	}
 };
