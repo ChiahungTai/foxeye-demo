cad901271f73e4766d9e6ad0f48a7bd54b2526f3
Kaku Kuo <kakukogou@gmail.com>
2015-06-08 11:04:46 +0800

    Sample_Monitor_Multiple_ProcessorWorker v2.1 show FPS

--- Sample_Monitor_Multiple_ProcessorWorker/control_worker.js
+++ Sample_Monitor_Multiple_ProcessorWorker/control_worker.js
@@ -6,9 +6,23 @@ var frameNum = 0;
 var dropNum = 0;
 
 function showDropRate() {
-	console.log("Drop rate = " + 100 * dropNum / frameNum + "%");
+	var dropRate = 100 * dropNum / frameNum + "%";
+	console.log("Drop rate = " + dropRate);
+	postMessage({"type":"display_dropRateInfo",
+							 "dropRate":dropRate});
 }
 
+function showFPS() {
+	var fps = 1000.0 * (frameNum - dropNum) / (currentTime - veryStartTime);
+	console.log("FPS = " + fps);
+	postMessage({"type":"display_fpsinfo",
+							 "fps":fps});
+}
+
+var isVeryStartTimeInitialized = false;
+var veryStartTime;
+var previousTime;
+var currentTime;
 
 onmessage = function(event) {
 	if (event.data.type == "init_process_worker") {
@@ -20,8 +34,17 @@ onmessage = function(event) {
 }
 
 onvideoprocess = function(event) {
+
+	currentTime = new Date().getTime();
+
+	if (!isVeryStartTimeInitialized) {
+		veryStartTime = currentTime;
+		isVeryStartTimeInitialized = true;
+	}
+
 	if (frameNum % 100 == 1) {
 		showDropRate();
+		showFPS();
 	}
 
   if (!workerPool.Process(frameNum++, event.inputImageBitmap)) {
