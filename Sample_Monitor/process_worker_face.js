658cdbddaa2fab3578be97c55cf916e041ff76ae
Kaku Kuo <kakukogou@gmail.com>
2015-06-17 16:36:27 +0800

    Sample_Monitor: fix the ASM.js module initialization timing issue

--- Sample_Monitor/process_worker_face.js
+++ Sample_Monitor/process_worker_face.js
@@ -1,3 +1,18 @@
+// var Module;
+var Module = {
+  onRuntimeInitialized: function() {
+    console.log("onRuntimeInitialized() !!!");
+
+    detect_face_and_draw = Module.cwrap('kaku_face_detection_demo', 'number', ['number','number', 'number', 'number', 'number', 'number']);
+    detect_face_getX = Module.cwrap('kaku_face_detection_get_faceX', 'number', []);
+    detect_face_getY = Module.cwrap('kaku_face_detection_get_faceY', 'number', []);
+    detect_face_getW = Module.cwrap('kaku_face_detection_get_faceW', 'number', []);
+    detect_face_getH = Module.cwrap('kaku_face_detection_get_faceH', 'number', []);
+
+    isASMRuntimeInitialized = true;
+  }
+};
+
 var detect_face_and_draw;
 var detect_face_getX;
 var detect_face_getY;
@@ -8,36 +23,35 @@ var minFaceWidth;
 var minFaceHeight;
 var scaleFactor;
 
-var isASMInitialized = false;
+var isFaceModuleInitialized = false;
+var isASMRuntimeInitialized = false;
 
-function initializeASM(imageWidth, imageHeight, bitmapBufferLength) {
-
+function initFaceModule(imageWidth, imageHeight, bitmapBufferLength) {
   bitmapBufferOffset = Module._malloc(bitmapBufferLength);
   minFaceWidth  = Math.min(imageWidth, imageHeight) / 4;
   minFaceHeight = Math.min(imageWidth, imageHeight) / 4;
   scaleFactor = 1.1;
 
-  isASMInitialized = true;
+  isFaceModuleInitialized = true;
 }
 
-function initFace() {
+function initializeASM() {
   importScripts("facedetection.js");
-  detect_face_and_draw = Module.cwrap('kaku_face_detection_demo', 'number', ['number','number', 'number', 'number', 'number', 'number']);
-  detect_face_getX = Module.cwrap('kaku_face_detection_get_faceX', 'number', []);
-  detect_face_getY = Module.cwrap('kaku_face_detection_get_faceY', 'number', []);
-  detect_face_getW = Module.cwrap('kaku_face_detection_get_faceW', 'number', []);
-  detect_face_getH = Module.cwrap('kaku_face_detection_get_faceH', 'number', []);
 }
 
 function processOneFrame_RGBA(bitmap) {
+  if (!isASMRuntimeInitialized) {
+    postMessage({"type":"operation_finished_without_result"});
+    return;
+  }
 
   // force to take RGBA format data
   // handle the convertion in the Gecko
   var bitmapFormat = "RGBA32";
   var bitmapBufferLength = bitmap.mappedDataLength(bitmapFormat);;
 
-  if (!isASMInitialized) {
-    initializeASM(bitmap.width, bitmap.height, bitmapBufferLength);
+  if (!isFaceModuleInitialized) {
+    initFaceModule(bitmap.width, bitmap.height, bitmapBufferLength);
   }
 
   var bitmapPixelLayout = bitmap.mapDataInto(bitmapFormat, Module.HEAPU8.buffer, bitmapBufferOffset, bitmapBufferLength);
@@ -65,7 +79,7 @@ onmessage = function(event) {
 
   if (event.data.type == "init") {
     console.log("init_face");
-    initFace();
+    initializeASM();
   } else if (event.data.type == "process_one_frame") {
     processOneFrame_RGBA(event.data.bitmap);
   }
